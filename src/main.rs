172a670742d04eb29c2d2edfd715a2f6545c217b
Dante Kelly <dante@dantekelly.com>
2024-11-20 22:54:28 -0800

    Add basic actix hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, HttpResponse, Responder, HttpServer, App};
+
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    println!("Sending hello world to client");
+
+    HttpResponse::Ok().body("Hello, world!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
