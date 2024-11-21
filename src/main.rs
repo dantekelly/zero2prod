05161e78a5fb6623cf8ca00a4ca8964a9e094162
Dante Kelly <dante@dantekelly.com>
2024-11-20 23:01:46 -0800

    Add greet function with json support

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
-use actix_web::{get, HttpResponse, Responder, HttpServer, App};
+use actix_web::{get, post, web, HttpRequest, HttpResponse, Responder, HttpServer, App};
+use serde::Deserialize;
 
+#[derive(Deserialize)]
+struct Greeting {
+    name: String,
+}
 
 #[get("/")]
 async fn hello() -> impl Responder {
@@ -8,11 +13,18 @@ async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello, world!")
 }
 
+#[post("/greet")]
+async fn greet(_: HttpRequest, json: web::Json<Greeting>) -> impl Responder {
+    let name = json.name.clone();
+    format!("Hello, {}!", name)
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .service(hello)
+            .service(greet)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
