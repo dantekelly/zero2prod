dc3a5a2c9696a5bb909b7a17759a71a690522991
Dante Kelly <dante@dantekelly.com>
2024-11-20 23:40:32 -0800

    Add health check integration test

--- src/main.rs
+++ src/main.rs
@@ -53,3 +53,21 @@ async fn main() -> std::io::Result<()> {
     .run()
     .await
 }
+
+#[cfg(test)]
+mod tests {
+    use actix_web::{test, App};
+
+    use super::*;
+
+    #[actix_web::test]
+    async fn health_check_succeeds() {
+        let app = test::init_service(App::new().service(health_check)).await;
+        let req = test::TestRequest::get()
+            .uri("/healthz")
+            .to_request();
+        let resp = test::call_service(&app, req).await;
+
+        assert!(resp.status().is_success())
+    }
+}
