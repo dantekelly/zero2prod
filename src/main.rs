4ecc8fa18d77835815d2b0417495c71f00ce7cc6
Dante Kelly <dante@dantekelly.com>
2024-11-20 23:16:55 -0800

    Add rudimentary logging and add additional greet route to adhere to book examples

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ struct Greeting {
 
 #[get("/")]
 async fn hello() -> impl Responder {
-    println!("Sending hello world to client");
+    println!("[INFO] Sending hello world to client");
 
     HttpResponse::Ok().body("Hello, world!")
 }
@@ -16,7 +16,21 @@ async fn hello() -> impl Responder {
 #[post("/greet")]
 async fn greet(_: HttpRequest, json: web::Json<Greeting>) -> impl Responder {
     let name = json.name.clone();
+    println!("[INFO] Greeting {} with hello", name); // TODO: Sanitize input
     format!("Hello, {}!", name)
+}
+
+#[get("/greet/{name}")]
+async fn greet_route(path: web::Path<String>) -> impl Responder {
+    let name = path.into_inner();
+
+    if name.is_empty() {
+        println!("[ERROR] Something went wrong!");
+        return "Unexpected error has occured.".to_string()
+    }
+
+    println!("[INFO] Greeting {} with hello", name);
+    format!("[DEPRECATED] Hello, {}!", name)
 }
 
 #[actix_web::main]
@@ -25,6 +39,7 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .service(hello)
             .service(greet)
+            .service(greet_route)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
