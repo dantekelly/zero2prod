13e3c08592cddd03f008bfff33e3a0fd2f5d3089
Dante Kelly <dante@dantekelly.com>
2024-11-30 22:35:21 -0800

    Update initialize db script

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -22,21 +22,26 @@ APP_DB_NAME="${APP_DB_NAME:=newsletter}"
 CONTAINER="rapidfort/postgresql"
 CONTAINER_NAME="postgres"
 
-# TODO: Change this to a check for the container already existing, atm if the container exists and is not running, it will fail.
-# Check if the container is already running
-if docker ps -f name="${CONTAINER_NAME}" --format '{{.ID}}' | grep -q .; then
-    echo "Container ${CONTAINER_NAME} is already running."
+# Check if the container exists and its status
+if docker ps -a -f name="${CONTAINER_NAME}" --format '{{.ID}}' | grep -q .; then
+    # Container exists, check if it's running
+    if docker ps -f name="${CONTAINER_NAME}" --format '{{.ID}}' | grep -q .; then
+        echo "Container ${CONTAINER_NAME} is already running."
+    else
+        echo "Container ${CONTAINER_NAME} exists but is not running. Starting..."
+        docker start "${CONTAINER_NAME}"
+    fi
 else
     docker run \
         --env POSTGRES_USER=${SUPERUSER} \
-    --env POSTGRES_PASSWORD=${SUPERUSER_PWD} \
-    --env POSTGRESQL_DATABASE=${APP_DB_NAME} \
-    --name "${CONTAINER_NAME}" \
-    --health-cmd="pg_isready -U ${SUPERUSER} || exit 1" \
-    --health-interval=1s \
-    --health-timeout=5s \
-    --health-retries=5 \
-    --publish "${DB_PORT}":5432 \
+        --env POSTGRES_PASSWORD=${SUPERUSER_PWD} \
+        --env POSTGRESQL_DATABASE=${APP_DB_NAME} \
+        --name "${CONTAINER_NAME}" \
+        --health-cmd="pg_isready -U ${SUPERUSER} || exit 1" \
+        --health-interval=1s \
+        --health-timeout=5s \
+        --health-retries=5 \
+        --publish "${DB_PORT}":5432 \
         --detach \
         ${CONTAINER}
 fi
@@ -53,8 +58,11 @@ done
 >&2 echo "Postgres is up and running on port ${DB_PORT}!"
 
 # Create the application user
+# TODO: Add a check to see if the user already exists, and if so, skip creating it.
 CREATE_QUERY="CREATE USER ${APP_USER} WITH PASSWORD '${APP_USER_PWD}';"
 docker exec -it "${CONTAINER_NAME}" psql -U "${SUPERUSER}" -c "${CREATE_QUERY}"
+
 # Grant create db privileges to the app user
+# TODO: Add a check to see if the user already has the privileges, and if so, skip granting them.
 GRANT_QUERY="ALTER USER ${APP_USER} CREATEDB;"
 docker exec -it "${CONTAINER_NAME}" psql -U "${SUPERUSER}" -c "${GRANT_QUERY}"
