02096bd1429a08e18e8548d4eb13b6d1a232cca9
Dante Kelly <dante@dantekelly.com>
2024-12-12 11:19:13 -0800

    Configure and test database in health_check

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,6 +1,40 @@
+use sqlx::{Connection, Executor, PgConnection, PgPool};
+use zero2prod::configuration::DatabaseSettings;
+
+pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
+    let maintenance_settings = DatabaseSettings {
+        database_name: "postgres".to_string(),
+        username: "postgres".to_string(),
+        password: "password".to_string(),
+        ..config.clone()
+    };
+
+    let mut connection = PgConnection::connect(&maintenance_settings.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
+    connection
+        .execute(format!(r#"CREATE DATABASE "{}""#, config.database_name).as_str())
+        .await
+        .expect("Failed to create database.");
+
+    let connection_pool = PgPool::connect(&maintenance_settings.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
+
+    sqlx::migrate!("./migrations")
+        .run(&connection_pool)
+        .await
+        .expect("Failed to migrate the database");
+
+    connection_pool
+}
+
 #[cfg(test)]
 mod tests {
+    use super::configure_database;
     use actix_web::{body, test, web, App};
+    use uuid::Uuid;
+    use zero2prod::configuration::get_configuration;
     use zero2prod::routes::healthz;
 
     #[actix_web::test]
@@ -15,5 +49,10 @@ mod tests {
         let bytes = body::to_bytes(body).await;
 
         assert_eq!(bytes.unwrap(), web::Bytes::from_static(b""));
+
+        // Check if the database is created and migrated
+        let mut configuration = get_configuration().expect("Failed to read configuration.");
+        configuration.database.database_name = Uuid::new_v4().to_string();
+        let _connection_pool = configure_database(&configuration.database).await;
     }
 }
